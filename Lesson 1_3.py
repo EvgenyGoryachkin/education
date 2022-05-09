f50e428a2d964fc339509cf0b909019af3757439
EvgenyGoryachkin <105202829+EvgenyGoryachkin@users.noreply.github.com>
2022-05-09 19:16:48 +0300

    Delete Lesson 1_3.py

--- Lesson 1_3.py
+++
@@ -1,5 +0,0 @@
-n = int(input("Укажите чему будет равно значение параметра n  -   "))
-amount = (n + int(str(n) +str(n)) + int(str(n) + str(n) + str(n)))
-print("Итого если значение расчитано как n+nn+nnn - %d" % amount)
-
-
