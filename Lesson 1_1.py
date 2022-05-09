7c3daa75952552736cd70e2fa36b79aaf6002a20
EvgenyGoryachkin <105202829+EvgenyGoryachkin@users.noreply.github.com>
2022-05-09 19:18:40 +0300

    Add files via upload

---
+++ Lesson 1_1.py
@@ -0,0 +1,13 @@
+a = 21
+b = 12
+c = 48
+print("Переменные a и b и с -", a, b, c)
+
+string1 = input("Укажите первый параметр ")
+number1 = int(input("Укажите первое число "))
+string2 = input("Укажите второй параметр ")
+number2 = int(input("Укажите второе число "))
+string3 = input("Укажите третий параметр ")
+number3 = int(input("Укажите третье число "))
+print("Указанные Вами результаты - %10s %5d %10s %5d %10s %5d" % (string1, number1, string2, number2, string3, number3))
+
