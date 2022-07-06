fcf735c632a83300fa819a1729ccf370621d8de3
EvgenyGoryachkin <105202829+EvgenyGoryachkin@users.noreply.github.com>
2022-07-06 09:09:28 +0300

    Add files via upload

---
+++ Al_5_3.py
@@ -0,0 +1,16 @@
+import numpy as np
+x0 = np.random.randn(10)
+x1 = np.random.randn(10)
+x2 = np.random.randn(10)
+x3 = np.random.randn(10)
+x4 = np.random.randn(10)
+x5 = np.random.randn(10)
+x6 = np.random.randn(10)
+x7 = np.random.randn(10)
+x8 = np.random.randn(10)
+x9 = np.random.randn(10)
+x_sum = x0 + x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9
+x_sum
+import matplotlib.pyplot as plt
+plt.hist(x_sum)
+plt.show()
